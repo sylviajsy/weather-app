56b43dd9cf2b9954c173b267cad2e39ad41b6c02
Siyi Ji <sylviajsyy@gmail.com>
2026-01-21 01:34:01 -0600

    Display wind speed

--- client/src/WeatherCard.jsx
+++ client/src/WeatherCard.jsx
@@ -5,11 +5,13 @@ const WeatherCard = ({ result }) => {
     <div className='weatherCard'>
         {/* City Name */}
       <h2>City : {result.name}, {result.sys.country}</h2>
-      {/* current weather icon, , and wind speed */}
+      {/* current weather icon, , and  */}
       {/* Display temperature*/}
       <h2>Temperature : {result.main.temp} F</h2>
       {/* Display humidity */}
       <h2>Humidity : {result.main.humidity}</h2>
+      {/* Display wind speed */}
+      <h2>Wind Speed : {result.wind.speed}</h2>
 
     </div>
   )

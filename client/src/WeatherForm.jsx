51a3a11fa0de686178c21e64ae9c4b2aa4dfb371
Siyi Ji <sylviajsyy@gmail.com>
2026-01-21 21:58:47 -0600

    Prevent refresh after submit

--- client/src/WeatherForm.jsx
+++ client/src/WeatherForm.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 const WeatherForm = () => {
+    const [input, setInput] = useState("");
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+  
   return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        <Input 
+
+        />
+        <button type="submit">Search</button>
+      </form>
     </div>
   )
 }

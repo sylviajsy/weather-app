4f46bf58b44a8add0b974f7c49510c3b42ee9c35
Siyi Ji <sylviajsyy@gmail.com>
2026-01-23 14:01:04 -0600

    Add zip API to backend

--- server/index.js
+++ server/index.js
@@ -9,21 +9,34 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/weather', async(req, res) => {
-    const city = req.query.cityName;
-    const apiKey = process.env.OPENWEATHER_API_KEY;
-    const params = new URLSearchParams({
+    const query = req.query.query;
+    // Regular Expression, if numbers, then zip code
+    const isZip = /^\d+$/.test(query);
+
+    let params;
+
+    if (isZip){
+        params = new URLSearchParams({
+            zip:`${query},US`,
+            appid:process.env.OPENWEATHER_API_KEY,
+            units:"imperial"
+        })
+    } else {
+        params = new URLSearchParams({
         // Client input
-        q:req.query.cityName,
+        q:query,
         // q:"Austin",
         appid:process.env.OPENWEATHER_API_KEY,
         units:"imperial"
     })
+    }
+    
     const url = `https://api.openweathermap.org/data/2.5/weather?${params}`
 
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log("Fetched data for:", city);
+        console.log("Fetched data for:", query);
         res.json(data);
     } catch(error){
         console.error(error);

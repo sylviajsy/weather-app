1ce98f75f352fc83121deb22bf6e8cdd14fa4c04
Siyi Ji <sylviajsyy@gmail.com>
2026-01-29 20:35:34 -0600

    Handling error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,10 @@ import WeatherForm from './WeatherForm.jsx';
 import WeatherCard from './WeatherCard.jsx';
 
 function App() {
-  const [input, setInput] = useState("Austin");
+  const [input, setInput] = useState("");
   const [weather, setWeather] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [unit, setUnit] = useState("F");
   
   // If there's data returned from child, we need to put it in ()
@@ -16,11 +17,25 @@ function App() {
 
   const loadInput = async (city) => {
     setLoading(true);
-    const response = await fetch(`http://localhost:8080/weather?query=${city}`);
-    const data = await response.json();
-    console.log("Data Received:", data);
-    setWeather(data);
-    setLoading(false);
+    setError(null);
+    setWeather(null);
+    // Catch error if data not received
+    try{
+      const response = await fetch(`http://localhost:8080/weather?query=${city}`);
+      const data = await response.json();
+
+      if (data.cod == "200"){
+        console.log("Data Received:", data);
+        setWeather(data);
+      } else {
+        setError(data.message)
+      }
+      
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   const toggleUnit = () => {
@@ -28,6 +43,8 @@ function App() {
   }
 
   useEffect(() => {
+    // If there's no input, return empty page
+    if (!input) return;
     loadInput(input);
     // Dependency Array: Whenever city value changes, render once
   }, [input]);
@@ -36,6 +53,7 @@ function App() {
     <>
       <h1>Techtonica Weather Forecast App</h1>
       <WeatherForm onSearch={onSearch} />
+      {error && <h2>Error:{error}</h2>}
       {weather && 
       <button onClick={toggleUnit}>
         Switch to {unit === "F" ? "°C" : "°F"}
@@ -43,7 +61,7 @@ function App() {
       {loading ? (
         <div>
         Loading... ⏳
-        </div>) : (<WeatherCard result={weather} unit={unit}/>)}
+        </div>) : weather && (<WeatherCard result={weather} unit={unit}/>)}
     </>
   )
 }

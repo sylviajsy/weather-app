9487d251ddf4a0f1b041f5b214a30173cd2e8413
Siyi Ji <sylviajsyy@gmail.com>
2026-03-18 00:26:24 -0500

    Add error handling package frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,20 @@ import { useState, useEffect } from 'react'
 import './App.css'
 // import LoginPage from "./components/LoginPage";
 import WeatherPage from "./components/WeatherPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   
   return (
     <>
+      <ToastContainer 
+          position="top-center"
+          autoClose={3000}
+          toastStyle={{
+            marginTop: "40vh",
+            textAlign: "center"
+          }}
+      />
       <WeatherPage />
     </>
   )

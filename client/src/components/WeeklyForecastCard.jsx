08c010e26444fee30f5011aeb87c8f81f5c2937b
Siyi Ji <sylviajsyy@gmail.com>
2026-03-24 21:45:00 -0500

    Add css to 5 day forecast

--- client/src/components/WeeklyForecastCard.jsx
+++ client/src/components/WeeklyForecastCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./WeeklyForecastCard.css"
 
 const WeeklyForecastCard = ({ weeklyForecast, unit }) => {
 
@@ -13,21 +14,26 @@ const WeeklyForecastCard = ({ weeklyForecast, unit }) => {
     <div className="weekly-grid">
         {weeklyForecast.map((day) => (
             <div className="weekly-card" key={day.dt}>
-                <h2 className="weekly-forecat-day"> {day.dt_txt.split(" ")[0]}, 
-                    {new Date(day.dt * 1000).toLocaleDateString("en-US", {
-                        weekday: "short",
-                    })}
-                </h2>
-                <h2 className="weekly-forecat-description">Description : {day.weather[0].description}</h2>
-                <img
-                    src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-                    alt={day.weather[0].description}
-                />
-                <h2 className="weekly-forecat-temp">
-				    Temperature : {convertTemp(day.main.temp)} °{unit}
-			    </h2>
-                <h2 className="weekly-forecat-humidity">Humidity : {day.main.humidity} %</h2>
-                <h2 className="weekly-forecat-wind-speed">Wind Speed : {day.wind.speed} mph</h2>
+                <div className="weekly-card-header">
+                    <h2> {day.dt_txt.split(" ")[0]}, 
+                        {new Date(day.dt * 1000).toLocaleDateString("en-US", {
+                            weekday: "short",
+                        })}
+                    </h2>
+                </div>
+
+                <div className='weekly-card-body'>
+                    <h2 className="weekly-description">Description : {day.weather[0].description}</h2>
+                    <img
+                        src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+                        alt={day.weather[0].description}
+                    />
+                    <h2 className="weekly-temp">
+                        Temperature : {convertTemp(day.main.temp)} °{unit}
+                    </h2>
+                    <h2 className="weekly-humidity">Humidity : {day.main.humidity} %</h2>
+                    <h2 className="weekly-wind-speed">Wind Speed : {day.wind.speed} mph</h2>
+                </div>
             </div>
         ))}
       

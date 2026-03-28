94f97ec059bd54338736444fbab905ec85895199
Siyi Ji <sylviajsyy@gmail.com>
2026-03-27 20:32:41 -0500

    DELETE fav API test

--- server/favorites.api.test.jsx
+++ server/favorites.api.test.jsx
@@ -49,4 +49,34 @@ describe('Favorites API test', () => {
         expect(response.status).toBe(201);
         expect(data.city_name).toBe("Boston");
     })
+
+    test('DELETE /api/fav/:id deletes a city', async () => {
+        const createRes = await fetch("http://localhost:8080/api/fav", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                cityName: "Dallas",
+            }),
+        });
+
+        const created = await createRes.json();
+
+        const deleteRes = await fetch(
+            `http://localhost:8080/api/fav/${created.id}`,
+            {
+                method: "DELETE",
+                headers: {
+                Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        const deleted = await deleteRes.json();
+
+        expect(deleteRes.ok).toBe(true);
+        expect(deleted.message).toMatch(/removed/i);
+    })
 })
